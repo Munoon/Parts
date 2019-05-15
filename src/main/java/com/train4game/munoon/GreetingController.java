135d4b1b010642976d3f0ba7fb0d8eb9470a63f0
Munoon <munoongg@gmail.com>
2019-05-15 16:12:54 +0300

    Added filter on main page

--- src/main/java/com/train4game/munoon/GreetingController.java
+++ src/main/java/com/train4game/munoon/GreetingController.java
@@ -36,6 +36,35 @@ public class GreetingController {
         return "index";
     }
 
+    @PostMapping
+    public String mainPostPage(
+            @RequestParam(name = "filter", required = false) String filter,
+            Map<String, Object> model
+    ) {
+        Iterable<Part> parts;
+
+        if (filter == null || filter.isEmpty()) {
+            parts = partRepo.findAll();
+        } else {
+            parts = partRepo.findByName(filter);
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        parts.forEach(part -> {
+            if (part.isNeed())
+                list.add(part.getCount());
+        });
+
+        if (list.size() == 0)
+            model.put("computers", 0);
+        else
+            model.put("computers", Collections.min(list));
+
+        model.put("parts", parts);
+        return "index";
+    }
+
     @GetMapping("/add")
     public String addGetPage() {
         return "add";
--- src/main/java/com/train4game/munoon/repos/PartRepo.java
+++ src/main/java/com/train4game/munoon/repos/PartRepo.java
@@ -3,5 +3,8 @@ package com.train4game.munoon.repos;
 import com.train4game.munoon.domain.Part;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PartRepo extends CrudRepository<Part, Long> {
+    List<Part> findByName(String name);
 }
