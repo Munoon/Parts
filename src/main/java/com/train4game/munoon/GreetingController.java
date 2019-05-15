46ed4cf480bf386aa2204a20c30d0dfa54c95282
Munoon <munoongg@gmail.com>
2019-05-15 15:50:30 +0300

    Added min computers count

--- src/main/java/com/train4game/munoon/GreetingController.java
+++ src/main/java/com/train4game/munoon/GreetingController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Controller
 public class GreetingController {
@@ -21,6 +20,18 @@ public class GreetingController {
             Map<String, Object> model
     ) {
         Iterable<Part> parts = partRepo.findAll();
+        List<Integer> list = new ArrayList<>();
+
+        parts.forEach(part -> {
+            if (part.isNeed())
+                list.add(part.getCount());
+        });
+
+        if (list.size() == 0)
+            model.put("computers", 0);
+        else
+            model.put("computers", Collections.min(list));
+
         model.put("parts", parts);
         return "index";
     }
